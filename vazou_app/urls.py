db093abcda7e7986103322a9870d90fe8331c469
Camile Alheiro <camilealheiro22@gmail.com>
2023-08-07 13:12:09 -0300

    BackEnd: Ponto de acesso com as informações da notícia

--- vazou_app/urls.py
+++ vazou_app/urls.py
@@ -1,8 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('empresas/', views.ListaEmpresa.as_view(), name='ListaEmpresa'),
+    path('noticias/', views.ListaNoticias.as_view(), name='ListaNoticias'),
     #path('Empresa/<int:pk>', views.index, name='index'),
     #path('api-auth/', include('rest_framework.urls')),
     #path('noticia/', views.rec_noticia, name='rec_noticia')
