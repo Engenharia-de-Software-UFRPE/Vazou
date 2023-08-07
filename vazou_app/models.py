22ef634a2c7f8b769f202ddc56d840c114209c98
Maria Gabrielly <mgabriellyanisio@gmail.com>
2023-08-07 02:23:26 -0300

    Atualização das tabelas e adição dos serializers

--- vazou_app/models.py
+++ vazou_app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
 from django.utils import timezone
+from rest_framework import serializers
 
 # Create your models here.
 class Empresa(models.Model):
@@ -29,10 +30,7 @@ class Empresa(models.Model):
     corporateName = models.CharField('Razão Social', max_length=100)
     abstract = models.TextField('Resumo', max_length=300)
     categoria = models.CharField(choices=CATEGORY, max_length=100)
-    score = models.DecimalField('Score',max_digits = 4, decimal_places = 2 , default=0)
-
-    #Relação - noticia
-    #Relação - ações da empresa
+    score = models.DecimalField('Score',max_digits = 4, decimal_places = 2 , default=0)    
 
     class Meta:
       db_table = 'empresa'
@@ -44,10 +42,9 @@ class Noticia(models.Model):
     text = models.TextField('Texto', max_length=3000)
     creation_date = models.DateTimeField('Data de criação', default=timezone.now)
     edit_date = models.DateTimeField('Última edição', default=timezone.now)
+    company = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='noticias_empresa')
     # ImagemTopo
     # ImagemTexto
-
-    # Relação - Empresa
 
     class Meta:
       db_table = 'noticia'
@@ -58,6 +55,28 @@ class Denuncia(models.Model):
    name_company = models.CharField('Nome da empresa', max_length=50)
    text = models.TextField('Texto', max_length=3000)
    dateTime = models.DateTimeField('Data e hora', default=datetime.now, blank=True)
+   company = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='acoes_empresa')
 
    class Meta:
       db_table = 'denuncia'
+
+class Acao(models.Model):
+   
+   title = models.CharField('Título da ação', max_length=200, unique=True)
+   text = models.TextField('Texto', max_length=3000)
+   dateTime = models.DateTimeField('Data e hora', default=datetime.now, blank=True)
+
+   class Meta:
+      db_table = 'acao'
+
+class EmpresaSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Empresa
+      fields = '__all__'
+
+class NoticiaSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Noticia
+      fields = '__all__'
+      related_company = "empresa"
+
