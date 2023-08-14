4c4a9cea79e71bb9b45e9642e43506d3dfd27d0c
Gabriel Leão <gabrielribeiroleao1301@gmail.com>
2023-08-14 01:19:42 -0300

    Notificação de denúncia

---
+++ vazou-frontend/app/components/modal.js
@@ -0,0 +1,28 @@
+import Button from "./button";
+
+export default function Modal ({leftButtonText, rightButtonText, title, setModalClosed, rightButtonAction}){
+
+    function closeModal (setModalClosed) {
+        setModalClosed ("notShow")
+    }
+    return (
+        <div className={'modal absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 drop-shadow-2xl'}>
+            <div className={'modal-content bg-white flex justify-center items-center flex-col px-12 py-8'}> 
+                <p className={'font-poppins mb-4'}>
+                    {title}
+                </p>
+                <div className={'flex justify-end'}> 
+                    <Button backgroundColor={"bg-orange-500"} onClickFunction={() => closeModal(setModalClosed)}>
+                        {leftButtonText}
+                    </Button>
+                    { rightButtonText !== undefined ?
+                        <Button backgroundColor={"bg-red-500"} onClickFunction={() => rightButtonAction()}>
+                            {rightButtonText}
+                        </Button> :
+                        ""
+                    } 
+                </div>
+            </div>     
+        </div>
+    )
+}
