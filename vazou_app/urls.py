e5bae4a27bff3b3045beb195465164aac162e17a
Maria Gabrielly <mgabriellyanisio@gmail.com>
2023-08-19 02:30:33 -0300

    BackEnd: Atualizações e Ponto de acesso

--- vazou_app/urls.py
+++ vazou_app/urls.py
@@ -7,13 +7,19 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('accounts/login/', admin.site.urls),
     path('', views.index, name='index'),
-    #path('empresas/', views.ListaEmpresa.as_view(), name='ListaEmpresa'),
-    #path('noticias/', views.ListaNoticias.as_view(), name='ListaNoticias'),
+
+    path('empresas/', views.ListaEmpresa.as_view(), name='ListaEmpresa'),
+    path('empresa/criar', views.CreateEmpresa.as_view(), name='CreateEmpresa'),
+
+
+    path('noticias/', views.ListaNoticia.as_view(), name='ListaNoticia'),
+    path('noticia/criar', views.CreateNoticia.as_view(), name='CreateNoticia'),
+
     #path('Empresa/<int:pk>', views.index, name='index'),
     path('api-auth/', include('rest_framework.urls')),
-    path('noticia', views.rec_noticia, name='rec_noticia'),
     path('denuncia', views.rec_report, name='rec_report'),
     path('send_email', views.send_email, name='send_email'),
+    
     path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
